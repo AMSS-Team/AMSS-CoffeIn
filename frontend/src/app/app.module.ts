a5784079bf5f500c810e99957fde63c7b0a6f3d9
anaoprea21 <ana.oprea@cst.ro>
2024-01-12 14:28:40 +0200

    Added check in

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GoogleMap, GoogleMapsModule} from "@angular/google-maps";
 import {ToastrModule} from "ngx-toastr";
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
@@ -14,7 +15,54 @@ import {ToastrModule} from "ngx-toastr";
     ToastrModule.forRoot({
       positionClass :'toast-bottom-right'
     })
+=======
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+// import angularfire
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { environment } from "../environments/environment.prod";
+import { FriendsTabComponent } from './components/friends-tab/friends-tab.component';
+import { HomeTabComponent } from './components/home-tab/home-tab.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CheckInComponent } from './components/check-in/check-in.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FriendsTabComponent,
+    HomeTabComponent,
+    CheckInComponent
+>>>>>>> Stashed changes
   ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        GoogleMapsModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-right'
+        }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFirestoreModule,
+        MatProgressSpinnerModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
